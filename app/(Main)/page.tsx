a5dd65ed58e34dc8e436406999359a9a256d11d5
BADHON512 <mylaptop512@gmail.com>
2025-01-21 23:51:59 +0600

    first commit

---
+++ app/(Main)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const RoutePage = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default RoutePage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Provider from "@/Provider/Provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <Provider>
+        <html lang="en">
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {children}
+          </body>
+        </html>
+      </Provider>
+      </ClerkProvider>
   );
 }
