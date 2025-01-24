8f385997132f8ec329432c63843c8faaf200f87e
BADHON512 <mylaptop512@gmail.com>
2025-01-24 23:38:43 +0600

    header 2.00

--- app/(Main)/_page.tsx
+++ app/(Main)/_page.tsx
@@ -2,18 +2,22 @@
 import SideBar from '@/components/SideBar/SideBar'
 import React from 'react'
 import HomeContent from '@/components/Content/HomeContent'
+import ForMobile from '@/components/SideBar/ForMobile'
 
 type Props = {}
 
 const Home = (props: Props) => {
 
   return (
-    <div className='flex '>
-        <div className='w-[279px] border-r border-[#262626] h-screen'>
+    <div className='md:flex '>
+        <div className='hidden md:block w-[79px] xl:w-[280px] h-screen'>
         <SideBar/>
 
         </div>
-        <div className="w-full h-screen">
+        <div className="fixed top-0 left-0 z-20 w-full block md:hidden">
+              < ForMobile/>
+        </div>
+        <div className="w-full h-screen mt-20">
              <HomeContent/>
         </div>
     </div>
