bfa8bb4999e0edd42f033c0bec71ada9e8b576e1
BADHON512 <mylaptop512@gmail.com>
2025-02-22 23:52:42 +0600

    find the all user today

---
+++ @actions/user/getAllUser.ts
@@ -0,0 +1,19 @@
+import prisma from "@/lib/prismaDb"
+import { cookies } from "next/headers"
+
+
+export async function GetAllUser() {
+    try {
+        const userId=await cookies()
+        const session=userId.get("session")?.value
+        if(!session){
+            return {error:"User not authenticated",statusCode:401}
+        }
+        const alluser=await prisma.user.findMany()
+
+    const users= alluser.filter((user)=>user.id!==session)
+    return {users,statusCode:200}
+    } catch (error) {
+        return {error,statusCode:500}
+    }
+}
