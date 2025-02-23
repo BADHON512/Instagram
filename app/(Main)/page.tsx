bd1a4e29b7806400ab6b5e6660351349d7b2105b
BADHON512 <mylaptop512@gmail.com>
2025-02-23 23:54:43 +0600

    fetch the all users

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -3,14 +3,16 @@ import Home from './_page';
 
 import { GetUser } from '@/@actions/user/getUser';
 import { GetAllPost } from '@/@actions/GetAllPost/GetAllPost';
+import { GetAllUser } from '@/@actions/user/getAllUser';
 
 const RoutePage = async () => {
  const   getUser:any=await GetUser()
  const Posts=await GetAllPost()
+ const users=await GetAllUser()
 
   return (
     <div>
-      <Home user={getUser?.user} Posts={Posts?.posts} />
+      <Home user={getUser?.user} Posts={Posts?.posts} users={users} />
     </div>
   );
 };
