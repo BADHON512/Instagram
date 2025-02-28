5aa7041691dd2f7af7e9223b397da952a4b9192d
BADHON512 <mylaptop512@gmail.com>
2025-02-28 23:53:41 +0600

    message page working in backend

---
+++ @actions/Message/createMessage.ts
@@ -0,0 +1,35 @@
+"use server";
+
+type Props = {
+  text: string;
+
+  receiverId: string;
+};
+import { cookies } from "next/headers";
+import prisma from "@/lib/prismaDb";
+
+export async function CreateMessage({ text, receiverId }: Props) {
+  try {
+    const getCookie = await cookies();
+    const userId = getCookie.get("userId")?.value;
+    if (!userId) {
+      return { message: "User ID not found", statusCode: 400 };
+    }
+
+    const createMessage = await prisma.message.create({
+      data: {
+        senderId: userId,
+        text: text,
+        receiverId: receiverId,
+      },
+    });
+
+    return {
+      createMessage,
+      message: "Message created successfully",
+      statusCode: 200,
+    };
+  } catch (error: any) {
+    return { error: error.message || "Internal Server Error", statusCode: 500 };
+  }
+}
